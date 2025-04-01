9d4a5dc742e601fc810ce0f52479087e39649f1f
KirillMoz <kirill.mozzhegorov@yandex.ru>
2025-04-01 22:13:44 +0500

    промежуточное закрытие

---
+++ WorkShop19.5/SocialNetwork/BLL/Services/FriendService.cs
@@ -0,0 +1,20 @@
+﻿using SocialNetwork.DAL.Repositories.Interfaces;
+using SocialNetwork.DAL.Repositories;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.DAL.Entities;
+
+namespace SocialNetwork.BLL.Services
+{
+    public class FriendService
+    {
+        IFriendRepository friendRepository;
+        IUserRepository userRepository;
+
+        public FriendService()
+        {
+            friendRepository = new FriendRepository();
+            userRepository = new UserRepository();
+        }
+    }
+}
